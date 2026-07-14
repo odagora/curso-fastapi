70a30f41de908e77058977aa55b91544ed1a05ea
Daniel <oscar.d.gonzalez@gmail.com>
2026-07-13 21:25:10 -0500

    add CustomerCreate and CustomerPublic models

--- models.py
+++ models.py
@@ -2,13 +2,23 @@ from typing import Annotated
 from pydantic import BaseModel, Field, EmailStr, computed_field
 
 
-class Customer(BaseModel):
+class CustomerBase(BaseModel):
     name: str
     description: str | None = None
     email: EmailStr
     age: Annotated[int, Field(gt=0, lt=120)]
     phone: str | None = None
     is_active: bool = True
+
+
+class CustomerCreate(CustomerBase):
+    """What the client sends to create a new customer."""
+
+
+class CustomerPublic(CustomerBase):
+    """What the server returns to client"""
+
+    id: int
 
 
 class Transaction(BaseModel):
@@ -19,13 +29,13 @@ class Transaction(BaseModel):
 
 class InvoiceRequest(BaseModel):
     id: int
-    customer: Customer
+    customer: CustomerPublic
     transactions: list[Transaction]
 
 
 class InvoiceResponse(BaseModel):
     id: int
-    customer: Customer
+    customer: CustomerPublic
     transactions: list[Transaction]
 
     @computed_field
